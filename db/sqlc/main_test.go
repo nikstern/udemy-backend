a90151ed4dd8108209753cc2abb78e07075c18ed
Nik Stern <nik.stern@datadoghq.com>
2023-08-07 12:35:32 -0400

    Added Viper for loading config variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:password@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/techschool/simplebank/db/util"
 )
 
 var testQueries *Queries
@@ -19,9 +15,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
 	if err != nil {
-		log.Fatal("Cannot connect to db: ", err)
+		log.Fatal("Cannot load config", err)
+	}
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db:", err)
 	}
 	testQueries = New(testDB)
 	os.Exit(m.Run())
